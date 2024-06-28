563f62a1fc2276f981d007f1c397a08ca9fd8841
MellowJoe <joethemellow@gmail.com>
2024-06-28 11:03:28 -0700

    auto-update, maybe

--- ReplaceAuthorWorks.user.js
+++ ReplaceAuthorWorks.user.js
@@ -1,5 +1,5 @@
 // Literotica - replace author works page
-// version 0.03
+// version 0.04
 //
 // --------------------------------------------------------------------
 //
@@ -14,7 +14,9 @@
 // @namespace     http://none.com/
 // @description   Replace author works page
 // @match       https://www.literotica.com/authors/*/works/*
-// @version 0.03
+// @version 0.04
+// @updateurl https://github.com/JoeTheMellow/ReplaceAuthorWorks/raw/main/ReplaceAuthorWorks.user.js
+// @downloadurl https://github.com/JoeTheMellow/ReplaceAuthorWorks/raw/main/ReplaceAuthorWorks.user.js
 // @grant GM_addStyle
 // @run-at document-end
 // ==/UserScript==
